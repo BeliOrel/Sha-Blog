c334f8838d84052188546229f9a2d7e69255b5ff
Beli Orel <anokasan@gmail.com>
2019-06-25 21:54:57 +0200

    Add Templates

--- Sha_Blog/shablog.py
+++ Sha_Blog/shablog.py
@@ -1,15 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Bee',
+        'title': 'Blog Post 1',
+        'content': 'First post content.',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Mei Honey',
+        'title': 'Blog Post 2',
+        'content': 'Second post content.',
+        'date_posted': 'April 25, 2018'
+    },
+    {
+        'author': 'Corey Bee',
+        'title': 'Blog Post 3',
+        'content': 'Third post content.',
+        'date_posted': 'May 20, 2018'
+    }
+]
 
 # you can have more than one route
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About!</h1>"
+    return render_template('about.html', title='About')
 
 
 # if we don't want to use environment variables
