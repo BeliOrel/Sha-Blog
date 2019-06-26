639b3a344983bc27758df8fddd0c2fcb808d98cf
Beli Orel <anokasan@gmail.com>
2019-06-27 00:54:36 +0200

    Forms and User Input

--- Sha_Blog/shablog.py
+++ Sha_Blog/shablog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+# secret key for aplication, so nobody can modify form data
+app.config['SECRET_KEY'] = '4cb195a017b7d117311173f6d1635375'
 
 posts = [
     {
@@ -31,6 +35,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))  # redirect to home page
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password!', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 # if we don't want to use environment variables
