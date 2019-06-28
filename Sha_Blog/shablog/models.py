79b93897f64a047dd1bb7523d66b6ce60bda5f71
Beli Orel <anokasan@gmail.com>
2019-06-28 20:10:42 +0200

    Add User Authentication

--- Sha_Blog/shablog/models.py
+++ Sha_Blog/shablog/models.py
@@ -1,8 +1,16 @@
 from datetime import datetime 
-from shablog import db
+from shablog import db, login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# UserMixin -> This provides default implementations 
+# for the methods that Flask-Login expects user objects to have.
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
