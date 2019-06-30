aff3d182f197f7435e0305c00994dfbe484058fd
Beli Orel <anokasan@gmail.com>
2019-06-30 19:30:35 +0200

    Create, Update and Delete posts

--- Sha_Blog/shablog/routes.py
+++ Sha_Blog/shablog/routes.py
@@ -1,40 +1,20 @@
 import os
 import secrets # for picture's name
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 # so we can use routes, DB and bcrypt -> @app
 from shablog import app, db, bcrypt
 from shablog.constants import PATH_PROFILE_PICTURE, DEFAULT_PROFILE_PICTURE
-from shablog.forms import RegistrationForm, LoginForm, UpdateAccount
+from shablog.forms import RegistrationForm, LoginForm, UpdateAccount, PostForm
 from shablog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-
-posts = [
-    {
-        'author': 'Corey Bee',
-        'title': 'Blog Post 1',
-        'content': 'First post content.',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Mei Honey',
-        'title': 'Blog Post 2',
-        'content': 'Second post content.',
-        'date_posted': 'April 25, 2018'
-    },
-    {
-        'author': 'Corey Bee',
-        'title': 'Blog Post 3',
-        'content': 'Third post content.',
-        'date_posted': 'May 20, 2018'
-    }
-]
 
 # you can have more than one route
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -129,3 +109,50 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/'+current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created.', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post', btn_label=0)
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        # you don't need to add to DB because it already exists there
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', post=post, form=form, legend='Update Post', btn_label=1)
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
